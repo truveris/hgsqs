aa5f64eb3031e3b4bdf3126f0da7d6eb8164b6d2
Bertrand Janin <b@janin.com>
2014-09-07 02:18:20 -0400

    Add metadata for pypi.

--- setup.py
+++ setup.py
@@ -3,15 +3,31 @@
 from setuptools import setup, find_packages
 
 
+here = os.path.abspath(os.path.dirname(__file__))
+
 setup(
     name="hgsqs",
     version="0.9.0",
     description="Mercurial to SQS hook",
+    long_description=open(os.path.join(here, 'README.md')).read(),
+    license="LICENSE.txt",
     author="Truveris Inc.",
     author_email="dev@truveris.com",
-    url="http://labs.truveris.com/",
+    url="https://github.com/truveris/hgsqs",
     install_requires=[
         "boto",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Software Development :: Version Control",
     ],
     packages=find_packages(),
 )
